6f24c617a1e51deec0a7c185482af70f75487ef0
gaurav patel <gaurav07patel@gmail.com>
2020-06-22 15:07:29 -0400

    calc.py with default input (8,4,2,1)

---
+++ calc.py
@@ -0,0 +1,21 @@
+
+def add(n1,n2,n3,n4):
+    addition =  n1+n2+n3+n4
+    print(addition)
+
+def sub(n1,n2,n3,n4):
+    subtraction = n1-n2-n3-n4 
+    print(subtraction)
+
+def mult(n1,n2,n3,n4):
+    multiplication = n1*n2*n3*n4 
+    print(multiplication)
+         
+def div(n1,n2,n3,n4):
+    division = n1/n2/n3/n4  
+    print(division)
+
+print(add(8,4,2,1))
+print(sub(8,4,2,1))
+print(mult(8,4,2,1))
+print(div(8,4,2,1))
